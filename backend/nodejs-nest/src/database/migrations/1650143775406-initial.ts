b1bd450755c052f29d514973c0ef845b50d33fae
danielbom <daniel.rodrigues.45@hotmail.com>
2022-06-30 00:59:38 -0300

    Recreate initial migration

--- backend/nodejs-nest/src/database/migrations/1650143775406-initial.ts
+++ backend/nodejs-nest/src/database/migrations/1650143775406-initial.ts
@@ -1,78 +1,74 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class initial1650143775406 implements MigrationInterface {
-  name = 'initial1650143775406';
-
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            CREATE TABLE \`user\` (
-                \`id\` varchar(36) NOT NULL,
-                \`username\` varchar(255) NOT NULL,
-                \`password\` varchar(255) NOT NULL,
-                \`role\` enum ('admin', 'user') NOT NULL,
-                \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-                \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-                \`deleted_at\` datetime(6) NULL,
-                PRIMARY KEY (\`id\`)
-            ) ENGINE = InnoDB
-        `);
+      CREATE TABLE \`user\` (
+        \`id\` varchar(36) NOT NULL,
+        \`username\` varchar(255) NOT NULL,
+        \`password\` varchar(255) NOT NULL,
+        \`role\` enum ('admin', 'user') NOT NULL,
+        \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+        \`deleted_at\` datetime(6) NULL,
+        PRIMARY KEY (\`id\`)
+      ) ENGINE = InnoDB
+    `);
     await queryRunner.query(`
-            CREATE TABLE \`post\` (
-                \`id\` varchar(36) NOT NULL,
-                \`content\` varchar(255) NOT NULL,
-                \`likes\` int NOT NULL,
-                \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-                \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-                \`deleted_at\` datetime(6) NULL,
-                \`author_id\` varchar(36) NULL,
-                PRIMARY KEY (\`id\`)
-            ) ENGINE = InnoDB
-        `);
+      CREATE TABLE \`post\` (
+        \`id\` varchar(36) NOT NULL,
+        \`content\` varchar(255) NOT NULL,
+        \`likes\` int NOT NULL,
+        \`author_id\` varchar(255) NOT NULL,
+        \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+        \`deleted_at\` datetime(6) NULL,
+        PRIMARY KEY (\`id\`)
+      ) ENGINE = InnoDB
+    `);
     await queryRunner.query(`
-            CREATE TABLE \`comment\` (
-                \`id\` varchar(36) NOT NULL,
-                \`content\` varchar(255) NOT NULL,
-                \`likes\` int NOT NULL,
-                \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-                \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-                \`deleted_at\` datetime(6) NULL,
-                \`post_parent_id\` varchar(36) NULL,
-                \`comment_parent_id\` varchar(36) NULL,
-                PRIMARY KEY (\`id\`)
-            ) ENGINE = InnoDB
-        `);
+      CREATE TABLE \`comment\` (
+        \`id\` varchar(36) NOT NULL,
+        \`content\` varchar(255) NOT NULL,
+        \`likes\` int NOT NULL,
+        \`author_id\` varchar(255) NOT NULL,
+        \`post_parent_id\` varchar(255) NOT NULL,
+        \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+        \`deleted_at\` datetime(6) NULL,
+        \`comment_parent_id\` varchar(36) NULL,
+        PRIMARY KEY (\`id\`)
+      ) ENGINE = InnoDB
+    `);
     await queryRunner.query(`
-            ALTER TABLE \`post\`
-            ADD CONSTRAINT \`FK_2f1a9ca8908fc8168bc18437f62\` FOREIGN KEY (\`author_id\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        `);
+      ALTER TABLE \`post\` ADD CONSTRAINT \`FK_2f1a9ca8908fc8168bc18437f62\` FOREIGN KEY (\`author_id\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    `);
     await queryRunner.query(`
-            ALTER TABLE \`comment\`
-            ADD CONSTRAINT \`FK_137a2718c6c677cb5e675c4e283\` FOREIGN KEY (\`post_parent_id\`) REFERENCES \`post\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        `);
+      ALTER TABLE \`comment\` ADD CONSTRAINT \`FK_3ce66469b26697baa097f8da923\` FOREIGN KEY (\`author_id\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    `);
     await queryRunner.query(`
-            ALTER TABLE \`comment\`
-            ADD CONSTRAINT \`FK_6a49447281e4d5525273b497a16\` FOREIGN KEY (\`comment_parent_id\`) REFERENCES \`comment\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        `);
+      ALTER TABLE \`comment\` ADD CONSTRAINT \`FK_137a2718c6c677cb5e675c4e283\` FOREIGN KEY (\`post_parent_id\`) REFERENCES \`post\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    `);
+    await queryRunner.query(`
+      ALTER TABLE \`comment\` ADD CONSTRAINT \`FK_6a49447281e4d5525273b497a16\` FOREIGN KEY (\`comment_parent_id\`) REFERENCES \`comment\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            ALTER TABLE \`comment\` DROP FOREIGN KEY \`FK_6a49447281e4d5525273b497a16\`
-        `);
+      ALTER TABLE \`comment\` DROP FOREIGN KEY \`FK_6a49447281e4d5525273b497a16\`
+    `);
     await queryRunner.query(`
-            ALTER TABLE \`comment\` DROP FOREIGN KEY \`FK_137a2718c6c677cb5e675c4e283\`
-        `);
+      ALTER TABLE \`comment\` DROP FOREIGN KEY \`FK_137a2718c6c677cb5e675c4e283\`
+    `);
     await queryRunner.query(`
-            ALTER TABLE \`post\` DROP FOREIGN KEY \`FK_2f1a9ca8908fc8168bc18437f62\`
-        `);
+      ALTER TABLE \`comment\` DROP FOREIGN KEY \`FK_3ce66469b26697baa097f8da923\`
+    `);
     await queryRunner.query(`
-            DROP TABLE \`comment\`
-        `);
-    await queryRunner.query(`
-            DROP TABLE \`post\`
-        `);
-    await queryRunner.query(`
-            DROP TABLE \`user\`
-        `);
+      ALTER TABLE \`post\` DROP FOREIGN KEY \`FK_2f1a9ca8908fc8168bc18437f62\`
+    `);
+    await queryRunner.query(`DROP TABLE \`comment\``);
+    await queryRunner.query(`DROP TABLE \`post\``);
+    await queryRunner.query(`DROP TABLE \`user\``);
   }
 }
