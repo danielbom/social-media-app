5714194506c7b6e7280a24ec3f374b348ac5f2ed
danielbom <daniel.rodrigues.45@hotmail.com>
2022-08-06 21:49:23 -0300

    Add .env support with pydantic.BaseSettings

--- backend/python-fastapi/app/env.py
+++ backend/python-fastapi/app/env.py
@@ -1,15 +1,34 @@
 import os
+from pydantic import BaseSettings
 
 from app.library.ms import parse_ms
 
-python_env = os.environ.get('PYTHON_ENV', 'development')
+# https://pydantic-docs.helpmanual.io/usage/settings/
+
+class Settings(BaseSettings):
+    python_env: str = 'development'
+
+    database_url: str = 'sqlite:///./database.db'
+    database_debug: bool = False
+
+    jwt_secret: str = 'secret'
+    jwt_algorithm: str = 'HS256'
+    jwt_expires_in: str = '1d'
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
+
+python_env = settings.python_env
 
 is_development = python_env == 'development'
 is_production = python_env == 'production'
 is_test = python_env == 'test'
 
-database_url = os.environ.get('DATABASE_URL', 'sqlite:///./database.db')
-database_debug = os.environ.get('DATABASE_DEBUG', 'False') == 'True'
+database_url = settings.database_url
+database_debug = settings.database_debug
 
-jwt_secret = os.environ.get('JWT_SECRET', 'secret')
-jwt_expires_in = parse_ms(os.environ.get('JWT_EXPIRES_IN', '1d'))
+jwt_secret = settings.jwt_secret
+jwt_expires_in = parse_ms(settings.jwt_expires_in)
