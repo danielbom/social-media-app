d7d0fdd53e625951632f9f264dc58e2999af13c4
daniel57060 <a57060@alunos.ipb.pt>
2024-01-16 16:37:39 +0000

    Refactor generate_api_test.py

--- scripts/py-api/tests/generate_api_test.py
+++ scripts/py-api/tests/generate_api_test.py
@@ -1,10 +1,29 @@
-import pytest
-
+from typing import List
 from pathlib import Path
-from generate_api.commons import Endpoint
+from generate_api.commons import Endpoint, ExternalTypeAction
 from generate_api.generate_api_python import generate_api_python
 from generate_api.generate_api_javascript import generate_api_javascript
 from generate_api.generate_api_typescript import generate_api_typescript
+
+import pytest
+
+FIXTURES = Path(__file__).parent / "fixtures"
+
+
+@pytest.mark.parametrize("language,external_types_action,expected_path", [
+    ("python", "export", FIXTURES / "fixture_single_file_python_export_expected.py"),
+    ("python", "import", FIXTURES / "fixture_single_file_python_import_expected.py"),
+    ("javascript", "ignore", FIXTURES / "fixture_single_file_javascript_expected.js"),
+    ("typescript", "export", FIXTURES /
+     "fixture_single_file_typescript_export_expected.ts"),
+    ("typescript", "import", FIXTURES /
+     "fixture_single_file_typescript_import_expected.ts"),
+])
+def test_generate_api(expected_endpoints: List[Endpoint], language: str, external_types_action: ExternalTypeAction, expected_path: Path):
+    generate_api = get_generate_api_from_language(language)
+    actual = generate_api(expected_endpoints, external_types_action)
+    expected = expected_path.read_text()
+    assert actual == expected
 
 
 @pytest.fixture
@@ -46,55 +65,12 @@ def expected_endpoints():
     return [Endpoint.from_dict(it) for it in endpoints]
 
 
-@pytest.fixture
-def expected_fixture_single_file_python_export():
-    return (Path(__file__).parent / "fixtures" / "fixture_single_file_python_export_expected.py").read_text()
-
-
-@pytest.fixture
-def expected_fixture_single_file_python_import():
-    return (Path(__file__).parent / "fixtures" / "fixture_single_file_python_import_expected.py").read_text()
-
-
-@pytest.fixture
-def expected_fixture_single_file_javascript():
-    return (Path(__file__).parent / "fixtures" / "fixture_single_file_javascript_expected.js").read_text()
-
-
-@pytest.fixture
-def expected_fixture_single_file_typescript_export():
-    return (Path(__file__).parent / "fixtures" / "fixture_single_file_typescript_export_expected.ts").read_text()
-
-
-@pytest.fixture
-def expected_fixture_single_file_typescript_import():
-    return (Path(__file__).parent / "fixtures" / "fixture_single_file_typescript_import_expected.ts").read_text()
-
-
-def test_generate_api_python_export(expected_fixture_single_file_python_export, expected_endpoints):
-    generated_result = generate_api_python(
-        expected_endpoints, external_types_action="export")
-    assert generated_result == expected_fixture_single_file_python_export
-
-
-def test_generate_api_python_import(expected_fixture_single_file_python_import, expected_endpoints):
-    generated_result = generate_api_python(
-        expected_endpoints, external_types_action="import")
-    assert generated_result == expected_fixture_single_file_python_import
-
-
-def test_generate_api_javascript(expected_fixture_single_file_javascript, expected_endpoints):
-    generated_result = generate_api_javascript(expected_endpoints)
-    assert generated_result == expected_fixture_single_file_javascript
-
-
-def test_generate_api_typescript_export(expected_fixture_single_file_typescript_export, expected_endpoints):
-    generated_result = generate_api_typescript(
-        expected_endpoints, external_types_action="export")
-    assert generated_result == expected_fixture_single_file_typescript_export
-
-
-def test_generate_api_typescript_import(expected_fixture_single_file_typescript_import, expected_endpoints):
-    generated_result = generate_api_typescript(
-        expected_endpoints, external_types_action="import")
-    assert generated_result == expected_fixture_single_file_typescript_import
+def get_generate_api_from_language(language: str):
+    if language == "python":
+        return generate_api_python
+    elif language == "javascript":
+        return generate_api_javascript
+    elif language == "typescript":
+        return generate_api_typescript
+    else:
+        raise ValueError(f"Unknown language: {language}")
